bf53ea7b08a68349174af311f2ac579b99be1da8
AbhikPatel <abhikpatel15@gmail.com>
2022-02-22 23:38:43 +0530

    feat: Responsive almost complete

--- angularTasks/src/app/app.component.ts
+++ angularTasks/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MainService } from './services/main.service';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  
   title = 'angularTasks';
+  window:boolean;
+
+  constructor(private ser:MainService) {
+  }
+
+  ngOnInit(): void {
+    this.window = false
+  }
+
 
 }
--- angularTasks/src/app/core/components/sidebar/sidebar.component.ts
+++ angularTasks/src/app/core/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MainService } from 'src/app/services/main.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ser:MainService) { }
 
   ngOnInit(): void {
+  }
+
+  menu()
+  {
+    this.ser.servar = false;
   }
 
 }
